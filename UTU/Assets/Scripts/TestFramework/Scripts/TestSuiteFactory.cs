a52378dc358228a300fc47b3f384f72b373433e0
Cyprien Larrouy <clarrouy@manzalab.com>
2017-10-16 15:05:43 +0200

    Added TestSuitefactory

---
+++ UTU/Assets/Scripts/TestFramework/Scripts/TestSuiteFactory.cs
@@ -0,0 +1,116 @@
+﻿
+
+public class TestSuiteFactory {
+
+    #region CONSTRUCTOR
+
+    public TestSuiteFactory () {
+        m_scriptsList = new System.Collections.Generic.List<string>();
+    }
+
+    #endregion
+
+    #region PUBLIC
+
+    public string GetFolderPath (string folderName) {
+        var directories = System.IO.Directory.GetDirectories(UnityEngine.Application.dataPath, folderName, System.IO.SearchOption.AllDirectories);
+        if (directories.Length > 0) {
+            return directories[0];
+        }
+        else {
+            return "";
+        }
+    }
+
+    public System.Collections.Generic.List<string> GetTestFilesAt (string folderPath) {
+        m_scriptsList = new System.Collections.Generic.List<string>();
+        if (System.IO.Directory.Exists(folderPath)) {
+            var filePaths = System.IO.Directory.GetFiles(folderPath);
+            for (int i = 0; i < filePaths.Length; i++) {
+                var split = filePaths[i].Split('\\');
+                var fileName = split[split.Length - 1];
+                if (fileName.Contains(TEST_TAG) && !fileName.Contains(".meta")) {
+                    m_scriptsList.Add(filePaths[i]);
+                }
+            }
+        }
+        return m_scriptsList;
+    }
+
+    public string GetClassNameFromFile (string filePath) {
+        var lines = System.IO.File.ReadAllLines(filePath);
+        for (int i = 0; i < lines.Length; i++) {
+            var line = lines[i];
+            if (line.Contains("class")) {
+                return GetClassName(line);
+            }
+        }
+        return "";
+    }
+
+    public System.Collections.Generic.List<string> GetTestFunctionsNames (string filePath, string className) {
+        var functions = new System.Collections.Generic.List<string>();
+        var lines = System.IO.File.ReadAllLines(filePath);
+        for (int i = 0; i < lines.Length; i++) {
+            var line = lines[i];
+            if (line.Contains("public") && !line.Contains(className) && line.Contains(TEST_TAG)) {
+                functions.Add(GetFunctionName(line));
+            }
+        }
+        return functions;
+    }
+
+    string GetClassName (string line) {
+        return GetWordAfter(line, "class");
+    }
+
+    string GetFunctionName (string line) {
+        return GetWordAfter(line, "void");
+    }
+
+    string GetWordAfter (string line, string word) {
+        var split = line.Split(' ');
+        var i = System.Array.IndexOf(split, word);
+        if (split.Length > i + 1) {
+            return split[i + 1];
+        }
+        else {
+            return "";
+        }
+    }
+
+    public TestSuite GenerateSuite (string filepath) {
+        var className = GetClassNameFromFile(filepath);
+        var functions = GetTestFunctionsNames(filepath, className);
+        var suite = new TestSuite(className + " Suite");
+
+        for (int i = 0; i < functions.Count; i++) {
+            string[] args = new string[1] { functions[i] };
+            var testCase = System.Activator.CreateInstance(System.Type.GetType(className), args) as TestCase;
+            suite.Add(testCase);
+        }
+        return suite;
+    }
+
+
+    public TestSuite[] GetSuites (string folderName) {
+        var path = GetFolderPath(folderName);
+        var files = GetTestFilesAt(path);
+
+        TestSuite[] suites = new TestSuite[files.Count];
+        for (int i = 0; i < files.Count; i++) {
+            suites[i] = GenerateSuite(files[i]);
+        }
+
+        return suites;
+    }
+
+    #endregion
+
+    #region PRIVATE
+
+    System.Collections.Generic.List<string> m_scriptsList;
+    const string TEST_TAG = "Test";
+
+    #endregion
+}
---
+++ UTU/Assets/Scripts/TestFramework/Tests/TestSuiteFactoryTestCase.cs
@@ -0,0 +1,84 @@
+﻿
+public class TestSuiteFactoryTestCase : TestCase {
+
+    #region CONSTRUCTOR
+
+    public TestSuiteFactoryTestCase (string methodName) : base (methodName) {
+
+    }
+
+    #endregion
+
+
+    #region PUBLIC
+
+    public override void Setup () {
+        base.Setup();
+        m_suiteFactory = new TestSuiteFactory();
+    }
+
+    public void TestFindFolderPath () {
+        var path = m_suiteFactory.GetFolderPath("tata");
+        Assert(path == "");
+        path = m_suiteFactory.GetFolderPath("DummyTestsFolder");
+        Assert(path == UnityEngine.Application.dataPath + "\\Scripts\\TestFramework\\DummyTestsFolder");    
+    }
+
+    public void TestDetectingTestsFolder () {
+        var path = m_suiteFactory.GetFolderPath("DummyTestsFolder");
+        var paths = m_suiteFactory.GetTestFilesAt(path);
+        Assert(paths.Count == 1);
+        Assert(paths[0] == UnityEngine.Application.dataPath + "\\Scripts\\TestFramework\\DummyTestsFolder\\DummyTestCase.cs");
+    }
+
+
+    public void TestReadFunctionsInFile () {
+        var file = UnityEngine.Application.dataPath + "\\Scripts\\TestFramework\\Tests\\DummyTestCaseTest.cs";
+        var functions = m_suiteFactory.GetTestFunctionsNames(file, "DummyTestCaseTest");
+        Assert(functions.Count == 10);
+        Assert(functions[0] == "TestTestingPhases");
+    }
+
+    public void TestGenerateSuite () {
+        var file = UnityEngine.Application.dataPath + "\\Scripts\\TestFramework\\Tests\\DummyTestCaseTest.cs";
+        var suite = m_suiteFactory.GenerateSuite(file);
+        Assert(suite.GetName() == "DummyTestCaseTest Suite");
+        Assert(suite.Count == 10);
+    }
+
+    public void TestSuiteExecution () {
+        var file = UnityEngine.Application.dataPath + "\\Scripts\\TestFramework\\Tests\\DummyTestCaseTest.cs";
+        var suite = m_suiteFactory.GenerateSuite(file);
+        var result = new TestResult();
+        result = suite.Run(result);
+
+        var witnessSuite = new TestSuite ("DummyTestCaseTest WitnessSuite");
+        var witnessResult = new TestResult();
+        witnessSuite.Add(new DummyTestCaseTest("TestTestingPhases"));
+        witnessSuite.Add(new DummyTestCaseTest("TestPassingTestsAccounting"));
+        witnessSuite.Add(new DummyTestCaseTest("TestFailingTestsAccounting"));
+        witnessSuite.Add(new DummyTestCaseTest("TestDummyTestResultSummaryFormatting"));
+        witnessSuite.Add(new DummyTestCaseTest("TestActualTestResultSummaryFormatting"));
+        witnessSuite.Add(new DummyTestCaseTest("TestTestResultFailedSetupSummaryFormatting"));
+        witnessSuite.Add(new DummyTestCaseTest("TestBrokenSetupDetection"));
+        witnessSuite.Add(new DummyTestCaseTest("TestTestingPhasesWhenSetupFails"));
+        witnessSuite.Add(new DummyTestCaseTest("TestTestSuiteSummary"));
+        witnessSuite.Add(new DummyTestCaseTest("TestTestSuiteDetails"));
+        witnessSuite.Run(witnessResult);
+
+        Assert(result.GetTotalTestsCount() == witnessResult.GetTotalTestsCount());
+        Assert(result.GetPassingTestsCount() == witnessResult.GetPassingTestsCount());
+        Assert(result.GetDetails().Count == suite.Count);
+        for (int i = 0; i < suite.Count; i++) {
+            Assert(result.GetDetails()[i] == witnessResult.GetDetails()[i]);
+        }
+    }
+    #endregion
+
+
+    #region PRIVATE
+
+    TestSuiteFactory m_suiteFactory;
+
+    #endregion
+}
